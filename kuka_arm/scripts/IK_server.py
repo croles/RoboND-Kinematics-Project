2e823b2e1d110c73831e8e4f6183db00b9a34df1
Antonio Cámara <croles@gmail.com>
2017-07-30 09:50:36 +0200

    feat: Add inverse kinematics calculation
    Calculate wrist position. Angles q1, q2 and q3. And then angles q4, q5, and q6 using a geometric calculation.

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -26,24 +26,6 @@ def handle_calculate_IK(req):
         return -1
     else:
 		
-        ### Your FK code here
-        # Create symbols
-	#
-	#   
-	# Create Modified DH parameters
-	#
-	#            
-	# Define Modified DH Transformation matrix
-	#
-	#
-	# Create individual transformation matrices
-	#
-	#
-	# Extract rotation matrices from the transformation matrices
-	#
-	#
-        ###
-
         # Initialize service response
         joint_trajectory_list = []
         for x in xrange(0, len(req.poses)):
@@ -61,19 +43,60 @@ def handle_calculate_IK(req):
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
      
-            ### Your IK code here 
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	    #
-	    #
-	    # Calculate joint angles using Geometric IK method
-	    #
-	    #
-            ###
+            # Calculate joint angles using Geometric IK method
+            lx = cos(roll)*cos(pitch)
+            ly = sin(roll)*cos(pitch)
+            lz = -sin(pitch)
+
+            gripper_length = 0.303 #d6 +d7
+
+            wx = px-(gripper_length)*lx
+            wy = py-(gripper_length)*ly
+            wz = pz-(gripper_length)*lz
 		
-            # Populate response for the IK request
-            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+	    q1 = atan2(wy,wx)
+
+            #q2 position
+            x_2 = 0.35 * cos(q1)
+            y_2 = 0.35 * sin(q1)
+            z_2 = 0.75
+            a2 = 1.25
+            d3 = 1.5
+
+            #distance q2-q4
+            dist1 = sqrt((wx-x_2)**2.0 + (wy-y_2)**2.0 + (wz-z_2)**2.0)
+
+            q3 = acos((a2**2.0 + d3**2.0 - dist1**2.0)/(2.0*a2*d3))
+            q3 = (q3 - pi/2.0) * -1.0 #conversion to robot joint angle
+
+            q2= atan2(wz-z_2,sqrt((wx-x_2)**2 + (wy-y_2)**2)) + acos((a2**2.0 - d3**2.0 + dist1**2.0)/(2.0*a2*dist1)) 
+            q2 = (q2 - pi/2.0) * -1.0 #conversion to robot joint angle
+            
+            #Roll and pitch in wrist coordinates space
+            roll2 = roll - q1
+            pitch2 = pitch - (q2 + q3)
+            #End efector projection in axe
+            dgry = 0.303*cos(pitch2)*sin(roll2)
+            dgrz = 0.303*sin(pitch2)
+
+            #wrist angles
+            q4 = atan2(dgry, dgrz)
+            q5 = atan2(sqrt(dgry**2+dgrz**2),cos(pitch2)*cos(roll2))
+            #clip q5 to avoid forbbiden values
+            if q5 > 2.18:
+                q5 = 2.18
+            if q5 < -2.18:
+                q5 = -2.18
+
+            if pitch>-pi/2 and pitch<pi/2:
+                q6 = yaw - q4
+            else:
+                q6 = pi - (yaw - q4)
+		
+	    # Populate response for the IK request
+            joint_trajectory_point.positions = [q1, q2, q3, q4, q5, q6]
+            #rospy.loginfo("Angles: %s" % joint_trajectory_point.positions)
+            joint_trajectory_list.append(joint_trajectory_point)
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
